0ac99f8241343705e6097334e4d3475cb471af37
Jake Wise <jakewise93@gmail.com>
2016-05-20 18:16:04 +0100

    Updated copyright incorrectly generated by Xcode.

--- GCDependencyInjectionTests/GCDIExampleFactoryService.m
+++ GCDependencyInjectionTests/GCDIExampleFactoryService.m
@@ -2,7 +2,7 @@
 // GCDependencyInjection
 //
 // Created by Jake Wise on 17/05/2016.
-// Copyright (c) 2016 Jake Wise. All rights reserved.
+// Copyright (c) 2016 GroovyCarrot. All rights reserved.
 //
 // You are permitted to use, modify, and distribute this file in accordance with
 // the terms of the license agreement accompanying it.
